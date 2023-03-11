2bf41dbd6f6cbe5f2eb67b8ab1e0c1e3e26f2a1c
José Amílcar Ferreira Casimiro <jose.a.casimiro@gmail.com>
2023-03-11 22:40:35 +0000

    final version before tests

--- 2023-03-13/tpc1.py
+++ 2023-03-13/tpc1.py
@@ -1,6 +1,9 @@
 # ------------------------------
 # TPC 1 - José Casimiro (A51113)
 # ------------------------------
+
+output = []
+
 def read_matrix(filename):
     file = open(filename, encoding="utf-8")
     n_linhas = int(file.readline())
@@ -13,36 +16,43 @@ def read_matrix(filename):
     file.close()
     return game_matrix
 
-def dfs(matriz, x, y, visitas, max_x, max_y):
+def dfs(matriz, x, y, visitas, depth):
+    # se já foi visitado sair
     if visitas[x][y] == 1:
         return
-    
+    # marcar como visitado
     visitas[x][y] = 1
-
+    # variáveis auxiliares da dimensão da matriz
+    max_x = len(matriz)
+    max_y = len(matriz[0])
+    # mostra a coordenadas para depuração (pode ser comentado)
+    #print (str(x) + "," + str(y) + "," + matriz[x][y] + "," + str(depth), "," + str(visitas[x][y]))
+    # se a posição corrente conter um cardinal
     if matriz[x][y] == "#":
-        if x > 0:
-            dfs(matriz, x-1, y, visitas, max_x, max_y)
-        if x < max_x - 1:
-            dfs(matriz, x+1, y, visitas, max_x, max_y)
-        if y > 0:
-            dfs(matriz, x, y-1, visitas, max_x, max_y)
-        if y < max_y - 1:
-            dfs(matriz, x, y+1, visitas, max_x, max_y)
-    else:
-        if x < max_x - 1:
-            dfs(matriz, x+1, y, visitas, max_x, max_y)
+        if depth == 0:
+            output.append(1)
         else:
-            if y < max_y - 1:
-                dfs(matriz, 0, y+1, visitas, max_x, max_y)
+            output[len(output)-1] += 1
+        depth += 1
+        if y < max_y-1:
+            dfs(matriz, x, y+1, visitas, depth)
+        if x < max_x-1:
+            dfs(matriz, x+1, y, visitas, depth)
 
-    return 0
+    # continuar a pesquisar a matriz
+    if depth == 0:
+        for vx in range(max_x):
+            for vy in range(max_y):
+                if visitas[vx][vy] == 0:
+                    dfs(matriz, vx, vy, visitas, 0)
 
 def main():
     game_matrix = read_matrix("2023-03-13/tabela_tetris.txt")
-    rows_game_matrix = len(game_matrix)
-    columns_game_matrix = len(game_matrix[0])
-    visitas_matriz = [[0 for i in range(columns_game_matrix)] for j in range(rows_game_matrix)]
-    dfs(game_matrix, 0, 0, visitas_matriz, rows_game_matrix, columns_game_matrix)
+    visitas_matriz = [[0 for i in range(len(game_matrix[0]))] for j in range(len(game_matrix))]
+    dfs(game_matrix, 0, 0, visitas_matriz, 0)
+    print("Número de peças: " + str(len(output)))
+    print("Maior peça: " + str(max(output)))
+    print(output)
     #write file
 
 if __name__ == "__main__":
