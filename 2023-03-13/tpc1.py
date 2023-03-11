f6b5624c4d7b0ac86bd632c1ece7be0861dda5c5
José Amílcar Ferreira Casimiro <jose.a.casimiro@gmail.com>
2023-03-11 17:08:26 +0000

    visitado

--- 2023-03-13/tpc1.py
+++ 2023-03-13/tpc1.py
@@ -1,9 +1,8 @@
 # ------------------------------
 # TPC 1 - José Casimiro (A51113)
 # ------------------------------
-
 def read_matrix(filename):
-    file = open(filename)
+    file = open(filename, encoding="utf-8")
     n_linhas = int(file.readline())
     n_colunas = int(file.readline())
     game_matrix = [['' for i in range(n_colunas)] for j in range(n_linhas)]
@@ -14,9 +13,37 @@ def read_matrix(filename):
     file.close()
     return game_matrix
 
+def dfs(matriz, x, y, visitas, max_x, max_y):
+    if visitas[x][y] == 1:
+        return
+    
+    visitas[x][y] = 1
+
+    if matriz[x][y] == "#":
+        if x > 0:
+            dfs(matriz, x-1, y, visitas, max_x, max_y)
+        if x < max_x - 1:
+            dfs(matriz, x+1, y, visitas, max_x, max_y)
+        if y > 0:
+            dfs(matriz, x, y-1, visitas, max_x, max_y)
+        if y < max_y - 1:
+            dfs(matriz, x, y+1, visitas, max_x, max_y)
+    else:
+        if x < max_x - 1:
+            dfs(matriz, x+1, y, visitas, max_x, max_y)
+        else:
+            if y < max_y - 1:
+                dfs(matriz, 0, y+1, visitas, max_x, max_y)
+
+    return 0
+
 def main():
     game_matrix = read_matrix("2023-03-13/tabela_tetris.txt")
-    
+    rows_game_matrix = len(game_matrix)
+    columns_game_matrix = len(game_matrix[0])
+    visitas_matriz = [[0 for i in range(columns_game_matrix)] for j in range(rows_game_matrix)]
+    dfs(game_matrix, 0, 0, visitas_matriz, rows_game_matrix, columns_game_matrix)
+    #write file
 
 if __name__ == "__main__":
     main()
